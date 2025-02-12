5247ebd550b77eccf593115f57cfb34dca972f27
Muhammad Fathin Mughits <fathinmughits2@upi.edu>
2025-02-12 11:01:55 +0700

    Add files via upload

---
+++ BelajarNetBeansMeaven.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.belajarnetbeansmeaven;
+
+/**
+ *
+ * @author fathi
+ */
+public class BelajarNetBeansMeaven {
+
+    public static void main(String[] args) {
+        System.out.println("Hallo Word Muhammad Fathin Mughits dan 2308520 Saya Senang Belajar Java");
+    }
+}
